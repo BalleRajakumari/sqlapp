e6691cdb6eb69c26e0d41619892cf0f77b65588d
User <User@DESKTOP-AMTGTU7>
2023-02-07 14:17:23 +0530

    Add project files.

---
+++ sqlapp/Models/Class.cs
@@ -0,0 +1,22 @@
+﻿namespace sqlapp.Models
+{
+    public class Product
+    {
+        internal string ProductName;
+        internal int ProductId;
+
+        public int ProductID
+        {
+            get;set;
+        }
+        public string Productname
+        {
+            get;set;
+        }
+        public int Quantity
+        {
+            get;set;
+        }
+
+    }
+}
